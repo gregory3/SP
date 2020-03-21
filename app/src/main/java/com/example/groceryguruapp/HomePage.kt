0e127c4cb7421ceba5ae9e78631ef26549fe318e
partlows <partlows16@gmail.com>
2020-03-20 20:41:08 -0400

    Home Page UI

--- app/src/main/java/com/example/groceryguruapp/HomePage.kt
+++ app/src/main/java/com/example/groceryguruapp/HomePage.kt
@@ -15,10 +15,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CreateList.newInstance] factory method to
+ * Use the [HomePage.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CreateList : Fragment() {
+class HomePage : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,9 +36,16 @@ class CreateList : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_list, container, false)
+        return inflater.inflate(R.layout.fragment_home_page, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.createNewListButton).setOnClickListener {
+            findNavController().navigate(R.id.action_homePage_to_createList);
+        }
+    }
 
     companion object {
         /**
@@ -47,12 +54,12 @@ class CreateList : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment CreateList.
+         * @return A new instance of fragment HomePage.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            CreateList().apply {
+            HomePage().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
